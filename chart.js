ceef679fec51a827df7feffd2dc2b007f0ddd9b7
Erin <erinferrara@ERF.local>
2019-04-24 20:59:01 -0700

    add cumulative line, iterative building function

--- chart.js
+++ chart.js
@@ -1,5 +1,39 @@
-Chart.defaults.global.defaultFontFamily = 'Barlow Condensed'
-Chart.defaults.global.defaultFontColor = '#1C1C1C'
+Chart.defaults.global.defaultFontFamily = 'Barlow Condensed';
+Chart.defaults.global.defaultFontColor = '#1C1C1C';
+
+const data2018 = [
+  1033,
+  1044,
+  307,
+  329,
+  1041,
+  953,
+  249,
+  650,
+  876,
+  1086,
+  1642,
+  1039
+];
+
+const data2019 = [
+    440,
+    463,
+    530,
+    963
+];
+
+function cumulativeData(rawList) {
+  cumData = [rawList[0]]
+  for(let i=1; i < rawList.length; i++){
+    cumval = cumData[i-1] + rawList[i]
+    cumData = cumData.concat(cumval)
+  }
+  return cumData
+}
+
+const cumulativeData2018 = cumulativeData(data2018)
+const cumulativeData2019 = cumulativeData(data2019)
 
 const bookPagesData = {
   labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
@@ -7,40 +41,42 @@ const bookPagesData = {
     label: '2018',
     backgroundColor: '#E24104',
     borderColor: '#E24104',
-    fill: false,
-    lineTension: 0,
-    data: [
-      1033,
-      1044,
-      307,
-      329,
-      1041,
-      953,
-      249,
-      650,
-      876,
-      1086,
-      1642,
-      1039
-    ]
+    yAxisID: 'y-axis-1',
+    // fill: false,
+    // lineTension: 0,
+    data: data2018
   } , {
     label: '2019',
     backgroundColor: '#4B6680',
     borderColor: '#4B6680',
+    yAxisID: 'y-axis-1',
+    // fill: false,
+    // lineTension: 0,
+    data: data2019
+  } , {
+    label: '2018 cumulative',
+    type: 'line',
+    backgroundColor: 'rgb(226, 65, 4, 0.5)',
+    borderColor: 'rgb(226, 65, 4, 0.5)',
     fill: false,
-    lineTension: 0,
-    data: [
-      440,
-      463,
-      530,
-      963
-    ]
+    // lineTension: .1,
+    yAxisID: 'y-axis-2',
+    data: cumulativeData2018
+  } , {
+    label: '2019 cumulative',
+    type: 'line',
+    backgroundColor: 'rgb(75, 102, 128, 0.5)',
+    borderColor: 'rgb(75, 102, 128, 0.5)',
+    fill: false,
+    // lineTension: .1,
+    yAxisID: 'y-axis-2',
+    data: cumulativeData2019
   }]
 };
 
 const ctx = document.getElementById('bookPagesChart').getContext('2d');
 const chart = new Chart(ctx, {
-  type: 'line',
+  type: 'bar',
   data: bookPagesData,
   options: {
     responsive: true,
@@ -55,6 +91,25 @@ const chart = new Chart(ctx, {
     hover: {
       mode: 'nearest',
       intersect: false
+    },
+    scales: {
+      yAxes: [{
+        type: 'linear',
+        display: true,
+        position: 'left',
+        id: 'y-axis-1',
+        gridLines: {
+          drawOnChartArea: false
+        }
+      } , {
+        type: 'linear',
+        display: true,
+        position: 'right',
+        id: 'y-axis-2',
+        gridLines: {
+          drawOnChartArea: false
+        }
+      }]
     }
   }
 });
