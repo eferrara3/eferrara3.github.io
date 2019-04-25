d7922b9f5f875155bf061049b180a4feec86f724
Erin <erinferrara@ERF.local>
2019-04-25 07:47:34 -0700

    adjust coloring, render order for graph

--- chart.js
+++ chart.js
@@ -39,34 +39,34 @@ const bookPagesData = {
   labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
   datasets: [{
     label: '2018',
-    backgroundColor: '#E24104',
-    borderColor: '#E24104',
+    backgroundColor: 'rgb(226, 65, 4, 0.5)',
+    borderColor: 'rgb(226, 65, 4, 0.5)',
     yAxisID: 'y-axis-1',
     // fill: false,
     // lineTension: 0,
     data: data2018
-  } , {
-    label: '2019',
-    backgroundColor: '#4B6680',
-    borderColor: '#4B6680',
-    yAxisID: 'y-axis-1',
-    // fill: false,
-    // lineTension: 0,
-    data: data2019
-  } , {
+    } , {
     label: '2018 cumulative',
     type: 'line',
-    backgroundColor: 'rgb(226, 65, 4, 0.5)',
-    borderColor: 'rgb(226, 65, 4, 0.5)',
+    backgroundColor: '#E24104',
+    borderColor: '#E24104',
     fill: false,
     // lineTension: .1,
     yAxisID: 'y-axis-2',
     data: cumulativeData2018
   } , {
-    label: '2019 cumulative',
-    type: 'line',
+    label: '2019',
     backgroundColor: 'rgb(75, 102, 128, 0.5)',
     borderColor: 'rgb(75, 102, 128, 0.5)',
+    yAxisID: 'y-axis-1',
+    // fill: false,
+    // lineTension: 0,
+    data: data2019
+  } ,  {
+    label: '2019 cumulative',
+    type: 'line',
+    backgroundColor: '#4B6680',
+    borderColor: '#4B6680',
     fill: false,
     // lineTension: .1,
     yAxisID: 'y-axis-2',
